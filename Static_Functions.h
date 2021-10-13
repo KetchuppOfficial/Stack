5dcd889854b997d02c281aa90ef2ea53690a0ed5
KetchuppOfficial <83651035+KetchuppOfficial@users.noreply.github.com>
2021-10-13 17:43:27 +0300

    Delete Static_Functions.h

--- Static_Functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef STATIC_FUNCTIONS_H_INCLUDED
-#define STATIC_FUNCTIONS_H_INCLUDED
-
-static int Start_Initialization (struct Stack *stack_ptr);
-static int Stack_Resize_Up (struct Stack *stack_ptr);
-static int Stack_Resize_Down (struct Stack *stack_ptr);
-
-#endif // STATIC_FUNCTIONS_H_INCLUDED
