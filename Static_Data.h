b7263df08d38cf5ccf9b785de3fcb08bf155bf5d
KetchuppOfficial <83651035+KetchuppOfficial@users.noreply.github.com>
2021-10-16 23:28:52 +0300

    Update Static_Data.h

--- Static_Data.h
+++ Static_Data.h
@@ -1,13 +1,12 @@
 #ifndef STATIC_DATA_H_INCLUDED
 #define STATIC_DATA_H_INCLUDED
 
-static int    Start_Initialization (struct Stack *stack_ptr);
-static int    Stack_Resize_Up      (struct Stack *stack_ptr);
-static int    Stack_Resize_Down    (struct Stack *stack_ptr);
-static int    Stack_Dump           (struct Stack *stack_ptr, FILE *output);
-static int    Check_Canary         (struct Stack *stack_ptr);
-static int    Check_Hash           (struct Stack *stack_ptr);
-static hash_t Calc_Hash            (struct Stack *stack_ptr);
+static int    Stack_Resize_Up   (struct Stack *stack_ptr);
+static int    Stack_Resize_Down (struct Stack *stack_ptr);
+static int    Stack_Dump        (struct Stack *stack_ptr, FILE *output);
+static int    Check_Canary      (struct Stack *stack_ptr);
+static int    Check_Hash        (struct Stack *stack_ptr);
+static hash_t Calc_Hash         (struct Stack *stack_ptr);
 
 //-----------------------------------------------
 //Defining error reports
